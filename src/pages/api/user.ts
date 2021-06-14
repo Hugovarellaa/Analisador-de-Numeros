838691584acd6e8e6ae617e69860fcbcf0fce7bd
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-14 20:59:30 -0300

    1 API Routes no Next.js
    Estratégias de autenticação

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+//Seriço de atenticação 
+//JWT (Storage) 
+//Next Auth (Social)
+//Cognito , Auth0
+
 export default (request: NextApiRequest, response: NextApiResponse) => {
     const user = [
         { id: 1, name: 'hugo' },
